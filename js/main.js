ec62166ddfaa2721ba39c4a23891620ff30b12f1
Eglė Juškevičienė <88egle@gmail.com>
2020-12-22 10:14:45 +0200

    clock edit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
+import { Clock } from './components/clock/clock.js'
+
 import { progressBarData } from './data/progressBarData.js';
 import { renderProgressBar } from './components/progressBar/renderProgressBar.js';
 
 import { socialsData } from './data/socialsData.js';
 import { renderSocials } from './components/socials/renderSocials.js';
 
+const clock = new Clock('.hero .clock');
+
+clock.init();
 
 renderProgressBar('.left', progressBarData);
 
