42bd84bee4c0488c999a2f2ae51b80fac8797607
Lukas P <lukas.ring@gmail.com>
2020-12-27 19:41:17 +0200

    added toast messages

--- built/components/form/formValidation.js
+++ built/components/form/formValidation.js
@@ -1,18 +1,32 @@
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 import { Validator } from "./Validator.js";
+import { renderToast, ToastType } from "../toast/toast.js";
 export function formValidation() {
     var forms = Array.from(document.querySelectorAll("form.form"));
     var _loop_1 = function (form) {
         var validationInputs = Array.from(form.querySelectorAll("[data-validation"));
         var submitBtn = form.querySelector('button[type="submit"]');
+        var toastCloasers = [];
         submitBtn.addEventListener("click", function (e) {
             e.preventDefault();
+            toastCloasers.forEach(function (callback) { return callback(); });
+            toastCloasers = [];
             var validationResult = validateInputs(validationInputs);
             console.log(validationResult);
             if (allInputsValid(validationResult)) {
-                console.log("All inputs valid!");
+                var closeToast = renderToast("Success!", ToastType.success);
+                toastCloasers.push(closeToast);
+                form.reset();
             }
             else {
-                console.error("Form contains invalid inputs!");
+                var closeToasts = showErrorMessages(validationResult);
+                toastCloasers = __spreadArrays(closeToasts);
             }
         });
     };
@@ -20,6 +34,15 @@ export function formValidation() {
         var form = forms_1[_i];
         _loop_1(form);
     }
+}
+function showErrorMessages(validationResult) {
+    var invalidInputs = validationResult.filter(function (result) { return result.isValid === false; });
+    var toastCloasers = [];
+    for (var i = invalidInputs.length; i > 0; i--) {
+        var closeToast = renderToast(invalidInputs[i - 1].message, ToastType.error, i);
+        toastCloasers.push(closeToast);
+    }
+    return toastCloasers;
 }
 function allInputsValid(validationResults) {
     for (var _i = 0, validationResults_1 = validationResults; _i < validationResults_1.length; _i++) {
--- ts/components/form/formValidation.ts
+++ ts/components/form/formValidation.ts
@@ -1,7 +1,10 @@
 import { Validator, ValidationResult } from "./Validator.js";
+import { renderToast, ToastType } from "../toast/toast.js";
 
 export function formValidation() {
-  const forms = Array.from(document.querySelectorAll("form.form"));
+  const forms = Array.from(
+    document.querySelectorAll("form.form")
+  ) as HTMLFormElement[];
 
   for (let form of forms) {
     const validationInputs = Array.from(
@@ -9,20 +12,46 @@ export function formValidation() {
     ) as HTMLInputElement[];
     const submitBtn = form.querySelector('button[type="submit"]');
 
+    let toastCloasers: any[] = [];
+
     submitBtn.addEventListener("click", (e) => {
       e.preventDefault();
+      toastCloasers.forEach((callback) => callback());
+      toastCloasers = [];
+
       const validationResult: ValidationResult[] = validateInputs(
         validationInputs
       );
       console.log(validationResult);
 
       if (allInputsValid(validationResult)) {
-        console.log("All inputs valid!");
+        const closeToast = renderToast("Success!", ToastType.success);
+        toastCloasers.push(closeToast);
+        form.reset();
       } else {
-        console.error("Form contains invalid inputs!");
+        const closeToasts = showErrorMessages(validationResult);
+        toastCloasers = [...closeToasts];
       }
     });
   }
+}
+
+function showErrorMessages(validationResult: ValidationResult[]) {
+  const invalidInputs = validationResult.filter(
+    (result) => result.isValid === false
+  );
+  const toastCloasers: any = [];
+
+  for (let i = invalidInputs.length; i > 0; i--) {
+    const closeToast = renderToast(
+      invalidInputs[i - 1].message,
+      ToastType.error,
+      i
+    );
+    toastCloasers.push(closeToast);
+  }
+
+  return toastCloasers;
 }
 
 function allInputsValid(validationResults: ValidationResult[]) {
